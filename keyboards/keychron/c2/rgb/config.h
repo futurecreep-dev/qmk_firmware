552bbf395924b23377c9190cf16503c6b2679dbc
JAO1988 <jao1988@gmail.com>
2024-09-26 05:14:05 -0400

    Update config.h
    Changed definitions of config.h to match current tree of sn32_develop

--- keyboards/keychron/c2/rgb/config.h
+++ keyboards/keychron/c2/rgb/config.h
@@ -14,8 +14,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+#define MATRIX_UNSELECT_DRIVE_HIGH
+
 #define BACKLIGHT_LEVELS 8
-#define SN32_RGB_MATRIX_ROW_PINS { C0, C1, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, B6, B7, B8, B9, B10, B11 }
+#define SN32F2XX_RGB_MATRIX_ROW_PINS { C0, C1, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, B6, B7, B8, B9, B10, B11 }
+#define SN32F2XX_PWM_OUTPUT_ACTIVE_LEVEL SN32F2XX_PWM_OUTPUT_ACTIVE_HIGH
+#define SN32F2XX_RGB_OUTPUT_ACTIVE_LEVEL SN32F2XX_RGB_OUTPUT_ACTIVE_LOW
 
 #define LED_MAC_PIN         B15
 #define LED_WIN_PIN         B12
