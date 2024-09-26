26fa46957064ba63e5e3ab5c7ae30258ebdb2e56
JAO1988 <jao1988@gmail.com>
2024-09-26 13:59:32 -0400

    Keychron C2 SN32_Develop Corrections
    Removed c2/rgb/old folder as appended files are no longer needed, updated rgb.c with new functions for dip_switch_enable, and updated keymaps with appropriate QMK optimizations and dipswitch toggle function.

--- keyboards/keychron/c2/rgb/config.h
+++ keyboards/keychron/c2/rgb/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2024 NetUserGet
+Copyright 2024 SiphonedAnomally
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,12 +14,17 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#define MATRIX_UNSELECT_DRIVE_HIGH
+#define SN32F2XX_RGB_MATRIX_ROW_PINS { C0, C1, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, B6, B7, B8, B9, B10, B11 }
+#define SN32F2XX_PWM_OUTPUT_ACTIVE_LEVEL SN32F2XX_PWM_OUTPUT_ACTIVE_HIGH /* Parameter added for PWM Transistor Permutations */
+#define SN32F2XX_RGB_OUTPUT_ACTIVE_LEVEL SN32F2XX_RGB_OUTPUT_ACTIVE_LOW  /* Parameter added for RGB Transistor Permutations */
 
 #define BACKLIGHT_LEVELS 8
-#define SN32F2XX_RGB_MATRIX_ROW_PINS { C0, C1, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, B6, B7, B8, B9, B10, B11 }
-#define SN32F2XX_PWM_OUTPUT_ACTIVE_LEVEL SN32F2XX_PWM_OUTPUT_ACTIVE_HIGH
-#define SN32F2XX_RGB_OUTPUT_ACTIVE_LEVEL SN32F2XX_RGB_OUTPUT_ACTIVE_LOW
 
-#define LED_MAC_PIN         B15
-#define LED_WIN_PIN         B12
+#define LED_WIN_PIN B12
+#define LED_MAC_PIN B15
+
+// START LAYER DEFINITIONS
+#define WIN_BASE 0
+#define WIN_FN   1
+#define MAC_BASE 2
+#define MAC_FN   3
--- keyboards/keychron/c2/rgb/keymaps/default/keymap.c
+++ keyboards/keychron/c2/rgb/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 Dimitris Mantzouranis
+Copyright 2024 SiphonedAnomaly
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -20,12 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-enum layer_names {
-    MAC_BASE,
-    MAC_FN,
-    WIN_BASE,
-    WIN_FN,
-};
 
 #define KC_TASK LGUI(KC_TAB)        // Task viewer
 #define KC_FLXP LGUI(KC_E)          // Windows file explorer
@@ -55,25 +49,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     +-----------------------------------------------------------------------------------------------------------------+
     */
 
-    [WIN_BASE] = { // Windows base layout
+    [WIN_BASE] = LAYOUT_ansi( // Windows Base layout
         /*  0         1         2         3         4         5         6         7         8         9         10        11        12        13        14        15        16       17       18       19       20      */
-        {   KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NO,    KC_PSCR,  KC_CRTN,  RGB_TOG                                     },
-        {   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS },
-        {   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS },
-        {   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,   KC_NO,    KC_NO,    KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_NO   },
-        {   KC_LSFT,  KC_NO,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,    KC_RSFT,  KC_NO,    KC_UP,    KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PENT },
-        {   KC_LCTL,  KC_LGUI,  KC_LALT,  KC_NO,    KC_NO,    KC_NO,    KC_SPC,   KC_NO,    KC_NO,    KC_RALT,  KC_RGUI,  MO(3),    KC_NO,    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_NO,   KC_P0,   KC_PDOT, KC_NO   }
-    },
+		KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_PSCR,  KC_CRTN,  RGB_TOG,
+		KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS, KC_PAST, KC_PMNS,
+		KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
+		KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                                 KC_P4,    KC_P5,   KC_P6,
+		KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,              KC_P1,    KC_P2,   KC_P3,   KC_PENT,
+		KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                       KC_RALT,  KC_RGUI,  MO(WIN_FN),         KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,            KC_P0,   KC_PDOT ),
 
-    [WIN_FN] = { // Windows Fn overlay
+    [WIN_FN] = LAYOUT_ansi( // Windows FN Overlay
         /*  0         1         2         3         4         5         6         7         8         9         10        11        12        13        14        15        16       17        18        19        20      */
-        {   QK_BOOT,  _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,    BL_UP,  BL_DOWN,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_SNIP,  _______,  RGB_MOD, _______,  _______,  _______,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUI,  RGB_VAI,  RGB_SAI, RGB_M_TW, _______,  _______,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUD,  RGB_VAD,  RGB_SAD, RGB_M_X,  RGB_M_G,  RGB_M_T,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, RGB_M_SW, RGB_M_SN, RGB_M_K,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_SPI,  _______, RGB_M_P,  RGB_M_B,  RGB_M_R,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_RMOD, RGB_SPD,  RGB_MOD, _______,  _______,  _______,  _______ }
-    },
+		QK_BOOT,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FLXP,    BL_UP,  BL_DOWN,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  			KC_SNIP,  _______,  RGB_MOD,
+		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUI,  RGB_VAI,  RGB_SAI,  RGB_M_TW, _______,  _______,  _______,
+		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUD,  RGB_VAD,  RGB_SAD,  RGB_M_X,  RGB_M_G,  RGB_M_T,  _______,
+		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,			  _______,								  RGB_M_SW, RGB_M_SN, RGB_M_K,
+		_______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,			  _______,			  RGB_SPI,			  RGB_M_P,  RGB_M_B,  RGB_M_R,  _______,
+		_______,  _______,  _______,                                _______,                      _______,  _______,  _______,            _______,  RGB_RMOD, RGB_SPD,  RGB_MOD,		    _______,  _______ ),
 
 
 
@@ -93,23 +85,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     +-----------------------------------------------------------------------------------------------------------------+
     */
 
-    [MAC_BASE] = { // Mac base layout
+    [MAC_BASE] = LAYOUT_ansi( // Mac Base layout
         /*  0         1         2         3         4         5         6         7         8         9         10        11        12        13        14        15        16       17       18       19       20      */
-        {   KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_NO,    KC_MSCR,  KC_SIRI,  RGB_TOG                                     },
-        {   KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP, KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS },
-        {   KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN, KC_P7,   KC_P8,   KC_P9,   KC_PPLS },
-        {   KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NO,    KC_ENT,   KC_NO,    KC_NO,    KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_NO   },
-        {   KC_LSFT,  KC_NO,    KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,    KC_RSFT,  KC_NO,    KC_UP,    KC_NO,   KC_P1,   KC_P2,   KC_P3,   KC_PENT },
-        {   KC_LCTL,  KC_LALT,  KC_LGUI,  KC_NO,    KC_NO,    KC_NO,    KC_SPC,   KC_NO,    KC_NO,    KC_RGUI,  KC_RALT,  MO(1),    KC_NO,    KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_NO,   KC_P0,   KC_PDOT, KC_NO   }
-    },
+		KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,             KC_MSCR,  KC_SIRI,  RGB_TOG,
+		KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS, KC_PAST, KC_PMNS,
+		KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
+		KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,                                 KC_P4,    KC_P5,   KC_P6,
+		KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,              KC_P1,    KC_P2,   KC_P3,   KC_PENT,
+		KC_LCTL,  KC_LALT,  KC_LGUI,                                KC_SPC,                       KC_RGUI,  KC_RALT,  MO(MAC_FN),         KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT,            KC_P0,   KC_PDOT ),
 
-    [MAC_FN] = { // Mac Fn overlay
-        /*  0         1         2         3         4         5         6         7         8         9         10        11        12        13        14        15        16       17        18        19        20     */
-        {   QK_BOOT,  KC_BRID,  KC_BRIU,  KC_MSSN,  KC_FIND,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  KC_MSNP,  _______,  RGB_TOG, _______,  _______,  _______,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUI,  RGB_VAI,  RGB_SAI, RGB_M_TW, _______,  _______,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUD,  RGB_VAD,  RGB_SAD, RGB_M_X,  RGB_M_G,  RGB_M_T,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______, RGB_M_SW, RGB_M_SN, RGB_M_K,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_SPI,  _______, RGB_M_P,  RGB_M_B,  RGB_M_R,  _______ },
-        {   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_RMOD, RGB_SPD,  RGB_MOD, _______,  _______,  _______,  _______ }
-    }
+    [MAC_FN] = LAYOUT_ansi( // Mac FN Overlay
+        /*  0         1         2         3         4         5         6         7         8         9         10        11        12        13        14        15        16       17        18        19        20      */
+		QK_BOOT,  KC_BRID,  KC_BRIU,  KC_MSSN,  KC_FIND,    BL_UP,  BL_DOWN,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  			KC_MSNP,  _______,  RGB_MOD,
+		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUI,  RGB_VAI,  RGB_SAI,  RGB_M_TW, _______,  _______,  _______,
+		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RGB_HUD,  RGB_VAD,  RGB_SAD,  RGB_M_X,  RGB_M_G,  RGB_M_T,  _______,
+		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,			  _______,								  RGB_M_SW, RGB_M_SN, RGB_M_K,
+		_______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,			  _______,			  RGB_SPI,			  RGB_M_P,  RGB_M_B,  RGB_M_R,  _______,
+		_______,  _______,  _______,                                _______,                      _______,  _______,  _______,            _______,  RGB_RMOD, RGB_SPD,  RGB_MOD,		    _______,  _______ ),
 };
+
+/* Mac/Windows - Toggle Switch parameters */
+bool dip_switch_update_user(uint8_t index, bool active){
+    if(index == 0) {
+        if(active) { // Mac mode
+            layer_move(0);
+        } else { // Windows mode
+            layer_move(2);
+        }
+    }
+    return true;
+}
